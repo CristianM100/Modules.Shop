bc85714d6f03129be4f63638b94c5b27586baedc
CristianM100 <cristian.marinescu92@yahoo.com>
2025-02-16 20:48:03 +0100

    modify page for products and just import the Products as component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,25 @@
 import Products from '@/components/Products';
 
+import { products } from '@/lib/data/products'
+
+/*
+const HomePage = () => {
+  return <Products products={products} />;
+};
+
+export default HomePage;*/
+
+
+export default function Home() {
+  return (
+    <main>
+      <Products products={products} />
+    </main>
+  )
+}
+
+
+/*
 interface ProductType {
   id: string;
   name: string;
@@ -16,27 +36,31 @@ interface ProductType {
   }[];
 }
 
-const fetchProducts = async (page: number): Promise<{ results: ProductType[] }> => {
-  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000'; 
-  const response = await fetch(`${baseUrl}/products?page=${page}`);
+
+
+const fetchProducts = async (): Promise<{ results: ProductType[] }> => { 
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
+  const response = await fetch(`${baseUrl}/api/products`);
+  //const response = await fetch(`${baseUrl}/api/products?page=${page}`);
+
 
   if (!response.ok) {
     throw new Error('Failed to fetch products');
   }
-  return response.json();
-};
+  const products = await response.json();
+  return products;
+
+}
 
 const Page = async () => {
-  const { results: products } = await fetchProducts(1);
+ const { results: products } = await fetchProducts();
+// const products  = await fetchProducts(1);
+
   return <Products products={products} />;
 };
 
 export default Page;
-
-
-
-
-
+*/
 
 
 
@@ -46,6 +70,7 @@ interface ProductType {
   name: string;
   price: number;
   rating: number;
+  slug: string;
   description: string;
   images: {
     id: string;
@@ -54,7 +79,6 @@ interface ProductType {
       metadata: string;
     };
   }[];
-  slug: string;
 }
 
 const fetchProducts = async (page: number): Promise<{ results: ProductType[] }> => {
@@ -93,4 +117,6 @@ const Page: React.FC = () => {
   return <Products products={products} />;
 };
 
-export default Page;*/
+export default Page;
+*/
+
