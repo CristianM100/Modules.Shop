bbb9319cdc9b73ac0b6437ebf348946cc857e208
CristianM100 <cristian.marinescu92@yahoo.com>
2025-03-02 10:12:23 +0100

    edit it

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,11 +1,15 @@
-import Product from '@/components/Product'
 
-/*interface ProductType {
+import * as React from 'react'
+import Product from '@/components/Product';
+
+export interface ProductType {
   id: string;
   name: string;
   price: number;
   rating: number;
+  slug: string;
   description: string;
+  product:string;
   images: {
     id: string;
     file: {
@@ -15,21 +19,53 @@ import Product from '@/components/Product'
   }[];
 }
 
-const fetchProductBySlug = async (slug: string): Promise<ProductType> => {
-  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000'; 
-  const response = await fetch(`${baseUrl}/products/${slug}`);
-  
+export interface ProductProps {
+  product: ProductType 
+
+}
+
+async function fetchProductBySlug(): Promise<ProductType> { 
+  const response = await fetch(`http://localhost:3000/api/products`);
+
   if (!response.ok) {
     throw new Error('Failed to fetch product');
   }
+
   return response.json();
+}
+
+const Page = async ({ }: { params: { slug: string } }) => {
+  const product = await fetchProductBySlug();
+
+  return ( 
+    <main>
+      <Product product={product} />
+    </main>
+  );
 };
 
-const Page = async ({ params }: { params: { slug: string } }) => {
-  const product = await fetchProductBySlug(params.slug);
+export default Page;
 
-  return <Product product={product} />;
-};
 
-export default Page;*/
 
+/*
+export default async function ProductPage({ params }: Props) {
+  
+  const resolvedParams = await params; 
+  const product = products.find((product: { slug: string; }) => product.slug === resolvedParams.slug);
+
+  if (!product) {
+    notFound();
+  }
+
+  return (
+    <main>
+      <Product product={product} />
+    </main>
+  );
+}*/
+
+
+
+
+ 
