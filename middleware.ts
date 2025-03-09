0f560d3326125fe5079ea1208b9ffe62fef1f244
CristianM100 <cristian.marinescu92@yahoo.com>
2025-03-09 13:16:30 +0100

    modify the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,21 @@
+/*import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/client"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+*/
+
+
+
+
 import { clerkMiddleware } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
@@ -5,14 +23,9 @@ export default clerkMiddleware(() => {
   return NextResponse.next()
 })
 
-export const config = {
-  //matcher: '/((?!_next/image|_next/static|favicon.ico).*)'
-    // Skip Next.js internals and all static files, unless found in search params
-    
+export const config = {   
     matcher: [
-      // Skip Next.js internals and all static files, unless found in search params
       '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-      // Always run for API routes
       '/(api|trpc)(.*)',
     ],
 }
