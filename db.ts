f2341145c758b30658542cbe8a730022f58526cd
CristianM100 <cristian.marinescu92@yahoo.com>
2025-03-09 13:16:13 +0100

    create db

---
+++ db.ts
@@ -0,0 +1,35 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI!;
+
+interface MongooseConn {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let cached: MongooseConn = (global as any).mongoose;
+
+if (!cached) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const connect = async () => {
+  if (cached.conn) return cached.conn;
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGO_URI, {
+      dbName: "clerkauthv5",
+      bufferCommands: false,
+      connectTimeoutMS: 30000,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
