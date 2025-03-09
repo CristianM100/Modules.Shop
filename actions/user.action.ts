b44f07eb700d681581d08889b8583c31bfdd1ae2
CristianM100 <cristian.marinescu92@yahoo.com>
2025-03-09 13:16:57 +0100

    create user action

---
+++ actions/user.action.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use server'
+
+//import User from "@/lib/models/user.modal";
+import User from "@/modals/user.modal";
+import { connect } from "@/db";
+
+
+export async function createUser(user: any) {
+  try {
+    await connect();
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    console.log(error);
+  }
+}
