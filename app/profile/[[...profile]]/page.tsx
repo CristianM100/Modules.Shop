e51afb4e3296788be1e76adf2fbd85279770390f
CristianM100 <cristian.marinescu92@yahoo.com>
2025-04-02 11:30:33 +0200

    change profile

--- app/profile/[[...profile]]/page.tsx
+++ app/profile/[[...profile]]/page.tsx
@@ -3,19 +3,24 @@ import { redirect } from "next/navigation";
 import React from "react";
 import { auth, currentUser } from "@clerk/nextjs/server";
 
+
 const Profile = async () => {
+
   const { userId } = await auth();
-  const isAuth = !!userId;
   const user = await currentUser();
+  const isAuth = !!userId;
 
   if (!isAuth) {
     redirect("/");
   }
 
+  {/* The profile is working only through ngrok */}
+
   return (
     <div className="flex flex-col items-center justify-center mt-8">
-      <h1 className="text-2xl">{user?.username}</h1>
-      <UserProfile />
+     {/* <h1 className="text-2xl">{user?.username}</h1>
+      <UserProfile />*/}  
+      <div>Profil with my orders</div>
     </div>
   );
 };
