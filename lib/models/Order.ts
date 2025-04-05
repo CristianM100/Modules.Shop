71a53adb06fb8b70a8712e3495a28536f3a64f2b
CristianM100 <cristian.marinescu92@yahoo.com>
2025-04-05 15:55:42 +0200

    comment orderId

--- lib/models/Order.ts
+++ lib/models/Order.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const OrderSchema = new mongoose.Schema({
   userId: { type: String, required: true },
-  orderId: { type: String, unique: true },
+  //orderId: { type: String },
   items: [
     {
       id: String,
