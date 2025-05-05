7727a9f4179caa302a932da9ca17e82aec95e8e7
CristianM100 <cristian.marinescu92@yahoo.com>
2025-05-05 22:03:17 +0200

    remove shipping

--- app/api/calculate-totals/route.ts
+++ app/api/calculate-totals/route.ts
@@ -5,14 +5,12 @@ export async function POST(request: Request) {
   const { cart } = await request.json();
 
   const sub_total = cart.items.reduce((sum: number, item: any) => sum + item.price_total, 0);
-  const shipping = 10; 
   const tax = sub_total * 0.1; 
-  const total = sub_total + shipping + tax;
+  const total = sub_total + tax;
 
   return NextResponse.json({
     ...cart,
     sub_total,
-    shipping,
     tax,
     total,
   });
