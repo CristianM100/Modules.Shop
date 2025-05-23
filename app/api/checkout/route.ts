53392a2cf66ebb38f4a36981479b96ba2ab5333f
CristianM100 <cristian.marinescu92@yahoo.com>
2025-05-23 12:38:08 +0200

    change stripe

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
-import Stripe from "stripe";
+//import Stripe from "stripe";
+import { stripe } from '@/lib/stripe'; // Adjust path as needed
 
-
+/*
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2025-02-24.acacia',
-});
+});*/
 
 export async function POST(request: Request) {
   try {
