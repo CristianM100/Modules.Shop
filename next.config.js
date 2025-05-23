c4e240c8a39105fb08d1d00a4b172f50a4f55054
Cristian Marinescu <cristian.marinescu92@yahoo.com>
2025-05-23 21:08:01 +0200

    change next.config.js  to next.config.ts

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  env: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  },
-};
-
-module.exports = nextConfig; 
